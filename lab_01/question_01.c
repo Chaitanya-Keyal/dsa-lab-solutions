372d2659df4c9b383eb59c70ccb8495f392bed2b
okaybro <66475772+Chaitanya-Keyal@users.noreply.github.com>
2026-01-18 18:16:36 +0000

    feat: lab 1 question 1

---
+++ lab_01/question_01.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  struct Student {
+    int ID;
+    char Name[50];
+    float Marks[3];
+    float Average;
+  } students[N];
+  for (int i = 0; i < N; i++) {
+    scanf("%d %s %f %f %f", &students[i].ID, students[i].Name,
+          &students[i].Marks[0], &students[i].Marks[1], &students[i].Marks[2]);
+    students[i].Average =
+        (students[i].Marks[0] + students[i].Marks[1] + students[i].Marks[2]) /
+        3.0;
+  }
+  printf("Merit List:\n");
+  for (int i = 0; i < N; i++) {
+    if (students[i].Average > 75.0) {
+      printf("%d %s (Avg: %.2f)\n", students[i].ID, students[i].Name,
+             students[i].Average);
+    }
+  }
+  struct Student topper = students[0];
+  for (int i = 1; i < N; i++) {
+    if (students[i].Average > topper.Average) {
+      topper = students[i];
+    }
+  }
+  printf("Topper: %s", topper.Name);
+
+  return 0;
+}
