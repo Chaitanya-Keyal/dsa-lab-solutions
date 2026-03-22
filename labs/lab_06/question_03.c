a1ee693a32ebcbf9969e8b61eb4e4fe8338538d4
okaybro <66475772+Chaitanya-Keyal@users.noreply.github.com>
2026-03-22 16:16:00 +0000

    feat: lab 6 question 3

---
+++ labs/lab_06/question_03.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+int stack[MAX];
+int top = -1;
+
+void push(int x) { stack[++top] = x; }
+
+int pop() { return stack[top--]; }
+
+int getTop() { return stack[top]; }
+
+int size() { return top + 1; }
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int heights[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &heights[i]);
+    }
+
+    int maxArea = 0;
+    for (int i = 0; i < n; i++) {
+        while (size() > 0 && heights[getTop()] >= heights[i]) {
+            int height = heights[pop()];
+            int width = size() == 0 ? i : i - getTop() - 1;
+            int area = height * width;
+            if (area > maxArea) {
+                maxArea = area;
+            }
+        }
+        push(i);
+    }
+    while (size() > 0) {
+        int height = heights[pop()];
+        int width = size() == 0 ? n : n - getTop() - 1;
+        int area = height * width;
+        if (area > maxArea) {
+            maxArea = area;
+        }
+    }
+    printf("%d\n", maxArea);
+
+    return 0;
+}
